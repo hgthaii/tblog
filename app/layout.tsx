64a5372893f55fac3507a98b42daf83b138612fc
hgthaii <hoangthai.txt@gmail.com>
2026-08-15 13:54:24 +0700

    fix(analytics): remove duplicate tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,13 +86,6 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
 			</body>
-
-			<script 
-				defer 
-				src={SITE_CONFIG.analytics.umami.src} 
-				data-website-id={SITE_CONFIG.analytics.umami.websiteId}
-			>
-			</script>
 		</html>
 	);
 }
